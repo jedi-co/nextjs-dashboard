f6f341683a6ac1a90b0f372d0ecd730cb1f5cc55
jedd.cox <jedd.cox@ifit.com>
2023-11-10 10:59:52 -0700

    removed the image

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,13 +4,6 @@ export default function Page() {
     return (
         <main>
             <p className="uppercase underline">Dashboard Page</p>
-            <Image
-                src="/customers/amy-burns.png"
-                width={100}
-                height={100}
-                className="hidden md:block"
-                alt="Screenshots of the dashboard project showing desktop and mobile versions"
-            />
         </main>
     );
 }
